b40899b958cbd42202d742c959780136b408d289
Valeri1963 <cvvei77@gmail.com>
2022-12-23 15:57:27 +0300

    add Hometask2

---
+++ Hometask2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int number = ReadInt("Введите число: ");
+int length = NumberLen(number);
+SumNumbers(number, length);
+
+
+int ReadInt(string message)      // ввод
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLen(int a)         // подсчет цифр
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+
+void SumNumbers(int n, int length)       // сумма
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
